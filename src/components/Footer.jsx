53ed54c5254089e62354c84c7f736a070dea8d1a
Aaima <aaimabibi27@gmail.com>
2025-07-19 23:03:45 +0500

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <h3>🍕 FoodMuse</h3>
-        <p>Crafted with code & cravings by Faiza. All rights reserved © {new Date().getFullYear()}</p>
+        <p>Crafted with code & cravings by Aaima. All rights reserved © {new Date().getFullYear()}</p>
 
       </div>
     </footer>
